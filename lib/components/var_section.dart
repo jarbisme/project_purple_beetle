07486354192ba830f219d1dd0c62ea6c5081ff33
Jose Angel Bautista Romero <52365128+jarb314@users.noreply.github.com>
2021-01-03 19:21:06 -0400

    Agregue el codigo para la seccion de variables

--- lib/components/var_section.dart
+++ lib/components/var_section.dart
@@ -10,9 +10,36 @@ class _VarSectionState extends State<VarSection> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 5.0),
+      margin: EdgeInsets.symmetric(vertical: 15.0),
       height: 60.0,
-      color: kPrimaryColor,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            width: 75.0,
+            child: OutlineButton(
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              color: kPrimaryColor,
+              highlightColor: kSecundaryColor.withOpacity(0.1),
+              highlightedBorderColor: kSecundaryColor.withOpacity(0.5),
+              splashColor: kSecundaryColor.withOpacity(0.3),
+              borderSide: BorderSide(color: kSecundaryColor, width: 2.5),
+              child: Text(
+                'var',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: kSecundaryColor,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
