dfbadf2f43e3cd7dbe592ec1fe965d05a931d474
Jose Angel Bautista Romero <52365128+jarb314@users.noreply.github.com>
2021-01-03 19:20:06 -0400

    Cree el widget para IconButton

--- lib/components/my_icon_button.dart
+++ lib/components/my_icon_button.dart
@@ -1,1 +1,27 @@
-// TODO: create icon button
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+import '../constants.dart';
+
+class MyIconButton extends StatelessWidget {
+  const MyIconButton({
+    Key key,
+    this.icon,
+  }) : super(key: key);
+
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        icon ?? FeatherIcons.alertCircle,
+        size: 25.0,
+        color: kPrimaryColor,
+      ),
+      onPressed: () {},
+      highlightColor: kPrimaryColor.withOpacity(0.1),
+      splashColor: kPrimaryColor.withOpacity(0.3),
+    );
+  }
+}
